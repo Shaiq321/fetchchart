62880870feee2c15beb1f68b82b8883a91655dc5
Shaiq321 <114628372+Shaiq321@users.noreply.github.com>
2024-03-18 02:06:19 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,65 @@
-import os
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-import telebot
+TOKEN: Final = '7160785140:AAHj-EdLG72nSdOtAmaP5SHzFdHF86RgIWg'
+BOT_USERNAME: Final ='@fetchchart_bot'
+# Commands
+async def start_command (update: Update, context: ContextTypes. DEFAULT_TYPE):
+ await update.message.reply_text('Hello! Thanks for chatting with me! I am a banana!')
 
-BOT_TOKEN = "7160785140:AAHj-EdLG72nSdOtAmaP5SHzFdHF86RgIWg"
+async def help_command(update: Update, context: ContextTypes. DEFAULT_TYPE):
+    await update.message.reply_text('I am a banana! Please type something so I can respond!')
 
-bot = telebot.TeleBot(BOT_TOKEN)
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+async def custom_command (update: Update, context: ContextTypes. DEFAULT_TYPE):
+ await update.message.reply_text('This is a custom command!')
 
+# Responses
+def handle_response(text: str) -> str:
+         processed: str = text.lower()
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+         if 'hello' in processed:
+            return 'Hey there!'
+       
+         if 'how are you' in processed:
+           return 'I am good!'
+       
+         if 'i love python' in processed:
+            return 'Remember to subscribe!'
+       
+         return 'I do not understand what you wrote...'
 
-bot.infinity_polling()
+async def handle_message(update: Update, context: ContextTypes. DEFAULT_TYPE):
+ message_type: str = update.message.chat.type
+ text: str = update.message.text
+ print(f'User ({update.message.chat.id}) in (message_type): "{text}"')
+ if message_type== 'group':
+  if BOT_USERNAME in text:
+    new_text: str = text.replace (BOT_USERNAME, '').strip()
+    response: str = handle_response(new_text)
+  else:
+   return 
+ else:
+  response: str = handle_response(text)
+  print('Bot:', response)
+ await update.message.reply_text(response)
+
+async def error (update: Update, context: ContextTypes.DEFAULT_TYPE):
+ print (f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+ print('Starting bot...')
+ app = Application.builder().token (TOKEN).build()
+# Commands
+ app.add_handler (CommandHandler('start', start_command))
+ app.add_handler(CommandHandler('help', help_command))
+ app.add_handler(CommandHandler('custom', custom_command))
+# Messages
+ app.add_handler(MessageHandler(filters.TEXT, handle_message))
+# Errors
+ app.add_error_handler(error)
+# Polls the bot
+ print('Polling...')
+ app.run_polling (poll_interval=6)
    
 
